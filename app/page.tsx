f3609bef1171ee7be029d2f33b739385da501974
FASami <foysal.developer@gmail.com>
2023-12-13 20:32:25 +0600

    chore: re-initiated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+import React from "react";
 
-export default function Home() {
+const Page = () => {
   return (
-    <h1>Hello</h1>
-  )
-}
+    <Link
+      style={{
+        height: "100dvh",
+        width: "100vw",
+        fontSize: "6vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#678",
+        textDecoration: "none",
+      }}
+      href="/"
+    >
+      dscript.art
+    </Link>
+  );
+};
+
+export default Page;
